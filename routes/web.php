6b90bbeaa871414d92179540dbc5b942fda636ba
BeainaT <tania.df@hotmail.it>
2022-07-19 15:37:29 +0200

    MVC Post in Admin+resource route+index

--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//back office public
 Route::get('/', function () {
     return view('welcome');
 });
 
+//back office authentication - public area
 Auth::routes();
 
-//set protected routes
+//set protected routes - back office private area
 Route::middleware('auth')
 ->namespace('Admin')
 ->name('admin.')
 ->prefix('admin')
 ->group(function() {
     Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
 });
 
 //set otherwise route - front office
